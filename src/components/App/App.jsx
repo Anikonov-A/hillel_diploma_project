c0ffb370d7e07e9705dd8c8cb160c8a646f239a0
Eugene270987 <anikevich.it@gmail.com>
2023-11-13 17:12:38 +0200

    Made: Title, Span, Paragraph components. Added favicon.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,25 @@
 import './App.css';
+import Title from '../Title/Title';
+import Paragraph from '../Paragraph/Paragraph';
+import Span from '../Span/Span';
 function App() {
   return (
       <div>
-
+          {/*While rendering a component - place {1-6} for size which will be equal to h1-h6. For more details - see Title.jsx. Also Look in Devtools, tab Elements, how it works*/}
+          <Title addClasses="d-flex" size={1}>
+              I am testing how all it works
+          </Title>
+          <Title addClasses="d-grid" size={2}>
+              I am testing how all it works
+          </Title>
+          {/*The same with paragraph - also make a component.*/}
+          <Paragraph addClasses="text-center">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium doloremque laudantium. Sed ut perspiciatis.
+          </Paragraph>
+          {/*The same for span*/}
+          <Span addClasses="text-center">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium doloremque laudantium. Sed ut perspiciatis.
+          </Span>
       </div>
   );
 }
