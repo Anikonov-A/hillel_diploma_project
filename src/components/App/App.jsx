51df07b2e8678bde0c7f8c0b6a2bb6e3dd40ae07
Eugene270987 <anikevich.it@gmail.com>
2023-11-14 11:11:09 +0200

    Created Example component where you can see how Title, Paragraph, span are built.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,11 @@
-import Title from '../Title/Title';
-import Paragraph from '../Paragraph/Paragraph';
-import Span from '../Span/Span';
 import './App.scss';
+import Example from '@/components/Example/Example';
 
 function App() {
   return (
       <div>
-          {/*While rendering a component - place {1-6} for size which will be equal to h1-h6. For more details - see Title.jsx. Also Look in Devtools, tab Elements, how it works*/}
-          <Title addClasses="d-flex" size={1}>
-              I am testing how all it works
-          </Title>
-          <Title addClasses="d-grid" size={2}>
-              I am testing how all it works
-          </Title>
-          {/*The same with paragraph - also make a component.*/}
-          <Paragraph addClasses="text-center">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium doloremque laudantium. Sed ut perspiciatis.
-          </Paragraph>
-          {/*The same for span*/}
-          <Span addClasses="text-center">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium doloremque laudantium. Sed ut perspiciatis.
-          </Span>
+          {/*See Example component*/}
+          <Example></Example>
       </div>
   );
 }
