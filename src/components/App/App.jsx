73c8c2b74834a0447b05ed73628bbe0988f9b424
Anton Anikonov <antonanikonov1@gmail.com>
2023-11-21 14:11:57 +0200

    Added prop-types for components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
 import './App.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
+import HomePage from "@/pages/HomePage/HomePage";
+import {NavButton} from "../Buttons/Buttons";
 
 function App() {
   return (
       <div>
          <Router>
-
-
+             <NavButton text="button" className="button green" to="$556"/>
+             <HomePage />
 
          </Router>
       </div>
