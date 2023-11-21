2a911b4145c3799b78d7baa24165ce660467d3c1
Anton Anikonov <antonanikonov1@gmail.com>
2023-11-21 19:06:50 +0200

    Created HomePage.jsx with hero section inside.Added new variables for them

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,13 @@
 import './App.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
+import HomePage from "@/pages/HomePage/HomePage";
 
 function App() {
   return (
       <div>
          <Router>
 
-
+           <HomePage />
 
          </Router>
       </div>
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import HeroSection from "./HeroSection/HeroSection";
+import "./HomePage.scss"
+
+export default function HomePage() {
+    return (
+        <main className="wrapper">
+            <HeroSection/>
+        </main>
+    )
+}
