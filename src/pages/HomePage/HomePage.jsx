192be77462e9be245ad1d385221a4f8934cf13ec
IraRadcenko <goncharovaira@ukr.net>
2023-11-23 15:33:07 +0200

    Created WhoWeAre section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import HeroSection from "./HeroSection/HeroSection";
+import WhoWeAre from "./WhoWeAreSection/WhoWeAreSection";
 import "./HomePage.scss"
 
 export default function HomePage() {
     return (
         <main className="wrapper">
             <HeroSection/>
+            <WhoWeAre/>
         </main>
     )
 }
