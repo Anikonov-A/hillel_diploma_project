97ce92c4fd98e2ec2265ca86b756f78a2ffe506e
Anton Anikonov <antonanikonov1@gmail.com>
2023-12-01 10:38:30 +0200

    created StyleGuide component.

--- src/pages/ServicePage/ServicePage.jsx
+++ src/pages/ServicePage/ServicePage.jsx
@@ -1,7 +1,12 @@
 import './ServicePage.scss';
+import StyleGuide from "../../components/StyleGuide/StyleGuide";
 
 function ServicePage () {
-
+  return(
+      <div className="wrapper">
+          <StyleGuide titleText="Services" backgroundClass="bg-services" />
+      </div>
+  )
 }
 
 export default ServicePage;
--- src/pages/TeamPage/TeamPage.jsx
+++ src/pages/TeamPage/TeamPage.jsx
@@ -1,9 +1,11 @@
 import './TeamPage.scss'
 import TeamSection from "../AboutPage/TeamSection/TeamSection";
+import StyleGuide from "../../components/StyleGuide/StyleGuide";
 
 function TeamPage () {
     return(
         <div className="wrapper">
+            <StyleGuide titleText="Our team" backgroundClass="bg-team" />
             <TeamSection />
         </div>
     )
