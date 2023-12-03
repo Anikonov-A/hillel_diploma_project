0962bc3ad4ef0c058a420726cd43974b636099b3
IraRadcenko <goncharovaira@ukr.net>
2023-12-03 18:35:39 +0200

    Added the TestimonialSection

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,6 +6,7 @@ import OfferSection from '../../components/OfferSection/OfferSection';
 import "./HomePage.scss"
 
 import OfferCardsSection from "./OfferCardsSection/OfferCardsSection";
+import Slider from "./TestimonialSection/TestimonialSection";
 
 export default function HomePage() {
     return (
@@ -13,6 +14,7 @@ export default function HomePage() {
             <HeroSection/>
             <OfferCardsSection />
             <AboutSection className='AboutSection'/>
+            <Slider/>
             <OfferSection/>
             <WhoWeAre/>
         </main>
