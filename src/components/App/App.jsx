a66bec0a82740d4833354d240c4e7e0403e88d1a
Anton Anikonov <antonanikonov1@gmail.com>
2023-12-05 18:26:04 +0200

    add imports file , restyled notify component.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,6 @@
 import './App.scss';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import HomePage from '../../pages/HomePage/HomePage';
-import AboutPage from '../../pages/AboutPage/AboutPage';
-import ShopPage from '../../pages/ShopPage/ShopPage';
-import ServicePage from '../../pages/ServicePage/ServicePage';
-import TeamPage from '../../pages/TeamPage/TeamPage';
-import ContactPage from '../../pages/ContactPage/ContactPage';
-import CartPage from '../../pages/CartPage/CartPage';
-import ErrorPage from '../../pages/ErrorPage/ErrorPage';
-import ChangeLogPage from '../../pages/ChangeLogPage/ChangeLogPage';
-import LicencesPage from '../../pages/LicencesPage/LicencesPage';
-import PasswordPage from '../../pages/PasswordPage/PasswordPage';
-import Subscription from '../Subscription/Subscription';
-import CategoryPage from '../CategoryPage/CategoryPage';
-import ProductPage from "../ProductPage/ProductPage";
-
+import { Header, Footer, HomePage, AboutPage, ShopPage, ServicePage, TeamPage, ContactPage, CartPage, ErrorPage, ChangeLogPage, LicencesPage, PasswordPage, Subscription, CategoryPage, ProductPage, } from "./imports"
 function App() {
     const handleSubscribe = (values) => {
         const SUBSCRIBE_KEY = "subscribeData";
