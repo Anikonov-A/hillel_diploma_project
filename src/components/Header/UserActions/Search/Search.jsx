dc878258ae3731f61e9fc3e5dc1ce34a4b310b99
Anton Anikonov <antonanikonov1@gmail.com>
2023-12-06 11:58:26 +0200

    Add search logic , add category string into all products. fixed issues

--- src/components/Header/UserActions/Search/Search.jsx
+++ src/components/Header/UserActions/Search/Search.jsx
@@ -1,21 +1,86 @@
 import './Search.scss';
-import {NavLink} from 'react-router-dom';
-import {paths} from '../../../../common/url';
+import {useState , useEffect} from "react";
+import axios from "axios";
+import {Link} from "react-router-dom";
 
-function Search () {
+
+const filterProducts = (searchText, listOfCategories,setListVisible) => {
+    if (!searchText) {
+        setListVisible(false);
+        return []
+    }
+    setListVisible(true);
+    return listOfCategories.reduce((acc, category) => {
+        const filteredItems = category.items.filter(({ name }) =>
+            name.toLowerCase().includes(searchText.toLowerCase())
+        );
+        return [...acc, ...filteredItems];
+    }, []);
+};
+
+function Search() {
+    const [searchData, setSearchData] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [productList, setProductList] = useState([]);
+    const [listVisible, setListVisible] = useState(true);
+
+
+    function operationWithSearch(){
+        window.scrollTo(0,0);
+        setListVisible(false);
+        setSearchTerm( "");
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+                const response = await axios.get('/data/data.json');
+                setSearchData(response.data.categories);
+
+        };
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        const debounce = setTimeout(() => {
+            const filteredProducts = filterProducts(searchTerm, searchData,setListVisible);
+            setProductList(filteredProducts);
+        }, 300);
+
+        return () => clearTimeout(debounce);
+    }, [searchTerm, searchData, setListVisible]);
+
     return (
         <div className="search">
             <form className="search__form">
                 <div className="search__wrapper">
-                    <label htmlFor="searchInput"></label>
-                    <input type="text" id="searchInput" className="search__input"/>
+                    <label htmlFor="searchInput">
+                        <img src="/images/search/search.svg" alt="search-icon" />
+                    </label>
+                    <input
+                        type="text"
+                        id="searchInput"
+                        value={searchTerm}
+                        className="search__input"
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        autoComplete="off"
+                    />
+                    <ul className={listVisible ? 'visible' : 'hidden'}  >
+                        {productList.length > 0 ? (
+                            productList.map((product, index) => (
+                                <li key={index}>
+                                    <Link to={`/product/${product.category}/${product.name.toLowerCase()}`} onClick={operationWithSearch}>
+                                        {product.name}
+                                    </Link>
+                                </li>
+                            ))
+                        ) : (
+                            <li>Nothing was found</li>
+                        )}
+                    </ul>
                 </div>
-                <NavLink to={paths.SHOP}>
-                    <img src="/images/search/search.svg" alt="search-icon"/>
-                </NavLink>
             </form>
         </div>
-    )
+    );
 }
 
 export default Search;
