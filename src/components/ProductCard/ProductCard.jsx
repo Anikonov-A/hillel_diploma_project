e11718827e5230b6ec2adcaa9cac51c92808f3b6
Anton Anikonov <antonanikonov1@gmail.com>
2023-12-07 19:30:25 +0200

    fix issues add scroll func to product card component

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -2,15 +2,18 @@ import './ProductCard.scss';
 import {Link} from 'react-router-dom';
 import {AddBtn} from '../Buttons/Buttons';
 function ProductCard ({data, category}) {
+    function scroll(){
+        window.scrollTo(0,0)
+    }
 
     return (
         <div className="offer__product">
-            <Link className="offer__product-category-link green" to={`/products/${category.toLowerCase()}`}>{category}</Link>
-            <Link className="offer__product-link" to={`/products/${category.toLowerCase()}/${data.name.toLowerCase()}`}>
+            <Link className="offer__product-category-link green" to={`/products/${category.toLowerCase()}`} onClick={scroll} >{category}</Link>
+            <Link className="offer__product-link" to={`/products/${category.toLowerCase()}/${data.name.toLowerCase()}`} onClick={scroll}>
                 <img className="offer__product-image" src={data.image} alt={data.name}/>
             </Link>
             <div className="offer__product-name">
-                <Link className="offer__product-name-link" to={`/products/${category.toLowerCase()}/${data.name.toLowerCase()}`}>
+                <Link className="offer__product-name-link" to={`/products/${category.toLowerCase()}/${data.name.toLowerCase()}`} onClick={scroll}>
                     {data.name}
                 </Link>
             </div>
