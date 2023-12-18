bd7b26fa881994616d15641c2fb94d2606f90458
Anton Anikonov <antonanikonov1@gmail.com>
2023-12-18 11:53:02 +0200

    Add related products section in product page

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -7,14 +7,14 @@ import {scrollTop} from "../../common/scrollFunction";
 import {toast} from "react-toastify";
 import React from "react";
 
-function ProductCard ({data, category}) {
+function ProductCard({data, category}) {
     const dispatch = useDispatch();
     const handleAddToCart = () => {
         dispatch(addItem(data));
-        toast.success('Product added to cart',{
-            className:"toast-modify",
-            position:"top-center",
-            containerId:"id4"
+        toast.success('Product added to cart', {
+            className: "toast-modify",
+            position: "top-center",
+            containerId: "id4"
         });
     };
 
@@ -22,7 +22,8 @@ function ProductCard ({data, category}) {
         <div className="offer__product">
             <Link className="offer__product-category-link green" to={`/products/${category.toLowerCase()}`}
                   onClick={scrollTop}>{category}</Link>
-            <Link className="offer__product-link" to={`/products/${category.toLowerCase()}/${data.name.toLowerCase()}`}
+            <Link className="offer__product-link"
+                  to={`/products/${category.toLowerCase()}/${data.name.toLowerCase()}`}
                   onClick={scrollTop}>
                 <img className="offer__product-image" src={data.image} alt={data.name}/>
             </Link>
@@ -37,7 +38,6 @@ function ProductCard ({data, category}) {
                 currency: 'USD'
             }).format(data.price)}</div>
             <AddBtn handler={handleAddToCart}/>
-
         </div>
     )
 }
